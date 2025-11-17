000f870db7ae3d9c25eb4e8ebde583a8ca1ac6aa
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-11-17 22:53:22 +0600

    Add state property

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react"
+
 function App() {
-    const sayHello = () => {
-        alert('Hello')
+    const [person, setPerson] = useState({
+        name: 'Root'
+    })
+
+    const changeName = () => {
+        const name = prompt('What is your name?')
+
+        if (name) {
+            setPerson({
+                name: name
+            })
+        }
     }
 
     return (
         <div className="min-h-screen bg-gray-100 p-4">
             <div className="bg-white w-1/2 border border-gray-200 my-2 p-3 mx-auto">
-                Ivan
+                <div class="mb-4">
+                    Name: {person.name}
+                </div>
+
+                <div>
+                    <button 
+                        className="inline-block text-xs text-white bg-sky-500 border border-sky-600 px-2 py-1 rounded cursor-pointer"
+                        onClick={() => changeName()}
+                    >Change name</button>
+                </div>
             </div>
+
+
         </div>
     )
 }
