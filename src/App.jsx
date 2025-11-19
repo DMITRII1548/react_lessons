a7241f6d4d8c33b33820850cd8264bf709972c33
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-11-19 21:59:06 +0600

    Add handling input

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
 import { useState } from "react"
 
 function App() {
-    const [person, setPerson] = useState({
-        name: 'Root'
+    const [post, setPost] = useState({
+        title: '',
+        content: '',
     })
 
-    const changeName = () => {
-        const name = prompt('What is your name?')
+    const handlePost = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        setPost({...post, [name]: value})
 
-        if (name) {
-            setPerson({
-                name: name
-            })
-        }
+        console.log(post)
     }
 
     return (
         <div className="min-h-screen bg-gray-100 p-4">
             <div className="bg-white w-1/2 border border-gray-200 my-2 p-3 mx-auto">
-                <div class="mb-4">
-                    Name: {person.name}
-                </div>
-
                 <div>
+                    <div className="mb-4">
+                        <input
+                            onChange={(e) => handlePost(e)}
+                            name="title"
+                            className="border border-gray-400 p-2 w-full"
+                            type="text"
+                            placeholder="Title"/>
+                    </div>
+                    <div className="mb-4">
+                        <textarea
+                            onChange={(e) => handlePost(e)}
+                            name="content"
+                            className="border border-gray-400 p-2 w-full"
+                            type="text"
+                            placeholder="Content">
+                        </textarea>
+                    </div>
                     <button 
                         className="inline-block text-xs text-white bg-sky-500 border border-sky-600 px-2 py-1 rounded cursor-pointer"
-                        onClick={() => changeName()}
                     >Change name</button>
                 </div>
             </div>
