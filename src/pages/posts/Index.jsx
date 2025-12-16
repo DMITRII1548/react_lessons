766f07c075cf33e208525e32a7f7c62b57b7c60e
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-12-16 21:32:04 +0600

    Add index posts action

--- src/pages/posts/Index.jsx
+++ src/pages/posts/Index.jsx
@@ -1,7 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 function Index() {
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async () => {
+        const res = await axios.get('http://localhost:3000/posts')
+        setPosts(res.data)
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
     return (
         <div>
-            Posts index
+            {posts.map(post => {
+                return (
+                    <div key={post.id} className="bg-white p-4 border border-gray-200 w-full mb-4">
+                        <strong>{post.title}</strong>
+                        <p>
+                            {post.content}
+                        </p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
